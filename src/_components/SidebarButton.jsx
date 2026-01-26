45801074e700f3a1a22c204fe28c56f0d22402fc
Nicolas <nicolasandreose@gmail.com>
2026-01-26 20:57:28 -0300

    refactor: add tailwind variant to sidebar

--- src/_components/SidebarButton.jsx
+++ src/_components/SidebarButton.jsx
@@ -1,15 +1,17 @@
+import { tv } from "tailwind-variants";
+
 const SidebarButton = ({ children, variant }) => {
-  const getVariantType = () => {
-    return variant === "selected"
-      ? "bg-[#E6F7F8] text-primary font-semibold"
-      : "";
-  };
+  const sidebarButton = tv({
+    base: "rounded-md px-6 py-3 text-lg font-normal",
+    variants: {
+      variant: {
+        selected: "bg-[#E6F7F8] text-primary font-semibold",
+      },
+    },
+  });
 
   return (
-    <a
-      href=""
-      className={`rounded-md px-6 py-3 text-lg font-normal ${getVariantType()}`}
-    >
+    <a href="" className={`${sidebarButton({ variant })}`}>
       {children}
     </a>
   );
